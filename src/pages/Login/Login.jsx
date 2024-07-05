2cf13d7044a3f049c1e119c9194839a0b6212a32
muhammad adhil <muhammadadhil934@gmail.com>
2024-07-05 21:53:18 +0530

    completed login page design

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,28 @@
 // import React from 'react'
 import './Login.css'
+import logo from '../../assets/logo.png'
 
 const Login = () => {
   return (
-    <div className='login'>
-      
-    </div>
-  )
+      <div className="login">
+          <img src={logo} className="login-logo" alt="" />
+          <div className="login-logo" alt=""></div>
+          <form action="" className="login-form">
+          <h1>Sign in </h1>
+              <input type="text" placeholder="Your Name" />
+              <input type="text" placeholder="Your Email" />
+              <input type="text" placeholder="Your Email" />
+              <button>Sign In</button>
+              <div className="form-help">
+                  <div className="remember">
+                      <input type="checkbox" />
+                      <label htmlFor="">Remember Me</label>
+                  </div>
+                  <p>Need Help?</p>
+              </div>
+          </form>
+      </div>
+  );
 }
 
 export default Login
